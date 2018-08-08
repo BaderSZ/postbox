7220a294112e1bd39ccf3f345018539a19c24b9c
Bader Zaidan <bader.zaidan@rwth-aachen.de>
2018-08-08 23:01:52 +0200

    Correct syntax and code

--- postbox.ino
+++ postbox.ino
@@ -1,8 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-#include <ESPMail.h>
 #include <string.h>
+
+/* TODO: SMTP. Plenty of broken, non-working libraries out there.
+   NOTE: deepSleep() wake (GPIO16->RST) seems not to work on my device.
+         RST->GND works, but no state saved.
+*/
 
 // Aliases. I prefer shorter, cleaner code.
 #define log         Serial.println
@@ -12,33 +16,27 @@
 #define NETSSID     "SSID"
 #define NETPASSWD   "PASSWORD"
 
-// Email data
-#define SERVER      "Server Host"
-#define PORT        465
-#define LOGIN       "Account Login"
-#define SENDER      "Sender Email"
-#define MAILPASSWD  "Account password"
 
-#define RECEIVER    "Recepient"
-
-/* NTPClient setup. IDE whines if it's not in this scope. */
+/* NTPClient setup. Logs to serial, will add to send.
+   timeclient requires localvar, server, and offset in sec.
+   GMT+2 atm, implement DST.
+*/
 WiFiUDP   ntpUDP;
-NTPClient timeClient(ntpUDP);
+NTPClient timeClient(ntpUDP, "ntp1.rwth-aachen.de", 2 * 60 * 60);
 
-
-/* connect to the WLAN defined above. This is done after wakeups. */
+// connect to the WLAN defined above. This is done after wakeups.
 void connectWLAN()
 {
   log("[connectWLAN] connecting to Network");
-  
-  WiFi.begin(NETSSID,NETPASSWD);
 
-  // error checking  
+  WiFi.begin(NETSSID, NETPASSWD);
+
+  // error checking
   while (WiFi.status() != WL_CONNECTED)
   {
     if (WiFi.status() == WL_CONNECT_FAILED)
       log("[connectWLAN] Failed to connect, trying again.");
-      delay(10000); 
+    delay(10000);
   }
 
   log("[connectWLAN] WiFi connected with IP: ");
@@ -46,33 +44,10 @@ void connectWLAN()
 }
 
 
-/* Send EMail with given data. */
-void sendMail(char* subject, char* message)
-{
-  
-  log("[sendMail] init and connect");
-  
-  ESPMail mail;
-  mail.begin();
+/*
+    setup() and loop().
+*/
 
-  mail.enableDebugMode();
-  
-  mail.setSubject(SENDER, subject);
-  mail.addTo(RECEIVER);
-  mail.setBody(message);
-  
-  log("[sendMail] SUBJET: " + (String)subject + " MSG: " + (String)message);
-  log("[sendMail] sending...");
-  
-  if (mail.send(SERVER, PORT, LOGIN, MAILPASSWD) == 0)
-    log("[sendMail] sent!");
-  else
-    log("[sendMail] sending failed.");
-}
-
-/*****
-  * Main/Mandatory functions. Loop for the sleep/wake loop, setup to initialise NTP *
-******/
 
 void setup() {
   // init console
@@ -84,22 +59,15 @@ void setup() {
   timeClient.begin();
 }
 
-
 void loop() {
-  connectWLAN();
-  sendMail("[Postman] Initialised", "Booted. Postman is now awake and sleeping until further notice.");
-  
-  while(true)
-  {
-    log("[loop] Booted! Sleeping until I have been awoken (again)...");
-    ESP.deepSleep(0); // sleep until wakeup
-    
-    // RESET! deepSleep() left and device awake.
+  // RESET! or boot.. deepSleep() left and device awake.
+  // TODO: Save state with a latching relay, EEPROM or the likes.
 
-    log("[loop] Someone woke me up!");
-    connectWLAN();
-    timeClient.update();
-    log((String)"[loop] Triggered at: " + gettime());
-    sendMail("[Postman] You've got mail!", "You've got mail at "); // TODO + gettime());
-  }
+  log("[loop] Someone woke me up or I have turned on for the first time!");
+  connectWLAN();
+  timeClient.update();
+  log((String)"[loop] Triggered at: " + gettime());
+  // TODO: SMTP Mail
+  ESP.deepSleep(0); // sleep until wakeup
 }
+
